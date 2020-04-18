ec76587eba2a3a0a5cc5a6b8f5766e1a8812f140
mukesh purohit <mpurohit88@gmail.com>
2020-04-19 03:29:03 +1245

    changes

--- mailer.js
+++ mailer.js
@@ -1,36 +1,23 @@
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(
-  "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"
-);
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 let contactUsEmail = (body) => {
-  const msg = {
-    to: "info.uniqueengineers@gmail.com,sales.uniqueengineers@gmail.com",
-    bcc: "prasant38@gmail.com",
-    from: {
-      email: "info.uniqueengineers@gmail.com",
-      name: "UNIQUE ENGINEERS",
-    },
-    subject: body.subject,
-    text: body.message,
-    html:
-      "Address: " +
-      body.address +
-      " <br />Message: " +
-      body.message +
-      " <br />mobileNo: " +
-      body.number +
-      " <br />email address: " +
-      body.email +
-      " <br />name: " +
-      body.name,
-  };
-  try {
-    sgMail.send(msg);
-  } catch (e) {
-    console.log("error...", e);
-  }
-};
+    const msg = {
+        to: 'info@unique-engr.com',
+        from: {
+            email: "info@unique-engr.com",
+            name: "UNIQUE ENGINEERS"
+        },
+        subject: body.subject,
+        text: body.message,
+        html: 'Address: ' + body.address + ' <br />Message: ' + body.message + ' <br />mobileNo: ' + body.number + ' <br />email address: ' + body.email + ' <br />name: ' + body.name,
+    };
+    try {
+        sgMail.send(msg);
+    } catch (e) {
+        console.log('error...', e);
+    }
+}
 module.exports = {
-  contactUsEmail,
-};
+    contactUsEmail
+}
